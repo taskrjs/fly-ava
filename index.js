3bff9d41cc9643df709e847f744559f1c9909546
Pine Mizune <pinemz@gmail.com>
2016-03-12 15:08:33 +0900

    fix(Api): move files arguments
    https://github.com/sindresorhus/ava/pull/536

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ module.exports = function () {
   this.ava = function (opts) {
     return this.unwrap(files => {
       return co(function *() {
-        const api = new Api(files)
+        const api = new Api()
         const reporter = new Verbose()
         reporter.api = api
 
         api.on('test', test => console.error(reporter.test(test)))
         api.on('error', error => console.error(reporter.unhandledError(test)))
 
-        yield api.run()
+        yield api.run(files)
 
         console.error(reporter.finish())
         if (api.failCount > 0) throw api.errors
