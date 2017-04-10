239dbd37aa66e1b572edce54d4d50c87a6345798
Pine Mizune <pinemz@gmail.com>
2017-04-11 00:57:30 +0900

    fix(package.json): support ava v0.19.1

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 'use strict'
 
-const format = require('path').format
+const path = require('path')
+const pkgConf = require('pkg-conf')
 
 const Api = require('ava/api')
 const Verbose = require('ava/lib/reporters/verbose')
+const babelConfigHelper = require('ava/lib/babel-config')
 
 module.exports = {
   name: 'ava',
   every: false,
   files: true,
   *func(files, opts) {
-    const api = new Api()
-    const reporter = new Verbose({ basePath: '.' })
+    const conf = yield pkgConf('ava')
+    const filepath = pkgConf.filepath(conf)
+    const projectDir = filepath === null ? process.cwd() : path.dirname(filepath)
+
+    const defaults = {
+      babelConfig: babelConfigHelper.validate(conf.babel),
+      projectDir,
+    }
+    opts = Object.assign({}, defaults, opts)
+
+    const api = new Api(opts)
+    const reporter = new Verbose()
 
     api.on('test', test => console.error(reporter.test(test)))
     api.on('error', error => console.error(reporter.unhandledError(test)))
 
-    const runStatus = yield api.run(files.map(format))
+    const runStatus = yield api.run(files.map(path.format))
 
     console.log(reporter.finish(runStatus))
     if (runStatus.failCount > 0) throw runStatus.errors
